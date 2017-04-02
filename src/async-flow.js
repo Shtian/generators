5c29054a4ba92cec44b139766840835cbe7e7cb4
Stian Haga <hagastian@gmail.com>
2017-04-02 17:38:20 +0200

    Add to async

--- src/async-flow.js
+++ src/async-flow.js
@@ -5,7 +5,8 @@ function * createPokemonFetcher () {
   const pokemonId = Math.floor((Math.random() * 151) + 1);
   const result = yield fetch(`${apiUrl}pokemon/${pokemonId}`);
   const pokemon = yield result.json();
-  return `This pokemon is named ${pokemon.name} and is of the type ${pokemon.types[0].type.name}`;
+  const types = pokemon.types.map(item => item.type.name).join('/');
+  return `🔍 Your random pokemon is named ${pokemon.name} and it is of the type(s) 👉 ${types}`;
 };
 
 const pokemonFetcher = createPokemonFetcher();
